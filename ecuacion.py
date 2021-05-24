ec6d3a2f82dc0f427447d27a3008a9aef0f62575
SMELG <guivalenator@gmail.com>
2021-05-24 16:34:45 -0500

    Fundamentos de Programacion

--- ecuacion.py
+++ ecuacion.py
@@ -2,6 +2,10 @@ a=2
 b=9
 c=10
 print("ecuacion a despejar")
+print("          2 _______")
+print("      -b±V² b²-4ac")
+print("X=--------------------")
+print("          2a        ")
 x1=-b+((b**2-(4*a*c))**2/(0.5))/(2*a)
 x2=-b-((b**2-(4*a*c))**2/(0.5))/(2*a)
 print(x1)
