beca046069be2fd1e6daaea197d0c774b22c76d3
smelg <guivalenator@gmail.com>
2021-05-24 21:45:31 -0500

    Principios de programacion

---
+++ ternario.py
@@ -0,0 +1,4 @@
+#Realiza un programa que asigne un valor entero usando un operador ternario así: 
+#si 5>3 asigne 2, en caso contrario asigne 4; luego, imprime el resultado en la línea final.
+ternario = 2 if 5>3  else 4
+print(ternario)
