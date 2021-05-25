b9364b25e2b16f67ecab57a0f5956e2e2f126064
SMELG <guivalenator@gmail.com>
2021-05-25 16:59:30 -0500

    Hello

--- ecuacion.py
+++ ecuacion.py
@@ -1,24 +1,20 @@
-#Priorizacion de operaciones
+#Definicion de variables
 a=2
 b=9
 c=10
+#Formula a usar
 print("ecuacion a despejar")
 print("          _________")
 print("      -b±V b²-4ac")
 print("X=--------------------")
 print("          2a        ")
-<<<<<<< HEAD
-x1=-b+((((b**2)-(4*a*c))**2) /0.5)/(2*a)
+#Calculo
 
-x3=(((b**2)-(4*a*c))**2)*0.5
+x1=-b+(((b**2)-(4*a*c))**0.5)/(2*a)
+x2=-b-(((b**2)-(4*a*c))**0.5)/(2*a)
+x3=-(((b**2)-(4*a*c))**0.5)
 
-x2=-b-((((b**2)-(4*a*c))**2) /0.5)/(2*a)
-
-=======
-x1=-b+((b**2)-(4*a*c)**0.5)/(2*a)
-x2=-b-((b**2)-(4*a*c)**0.5)/(2*a)
-x1=(-b+(b**2-4*a*c)**(0.5))/(2*a)
->>>>>>> beca046069be2fd1e6daaea197d0c774b22c76d3
+#Salida por pantalla
 print(x1)
 print(x2)
 print(x3)
--- variables.py
+++ variables.py
@@ -3,6 +3,7 @@ nombre="Luis Valencia"
 direcion="Calle 49 #40-209"
 telefono="3126272026"
 edad='47 años'
+
 #variables numericas
 #enteros
 documento=65331614
@@ -12,6 +13,9 @@ peso=92.7
 estatura=1.77
 #variables boolenas
 casado=True
+femenino=False
+
+# Salida por pantalla
 
 print("Mi nombre es:", nombre)
 print("Tengo",edad)
@@ -19,3 +23,7 @@ print("Mi direccion es: ",direcion)
 print("Mi telefono es: ",telefono)
 print("Mi documento es: ",documento)
 print("Eres casado: ",casado)
+print("Numero de curso: ", curso)
+print("Peso actual: ",peso)
+print("Estatura en metros: ",estatura)
+print("Es mujer: ",femenino)
