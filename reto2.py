8a54845fe8dce871d55d848bc48bfcc826650cf0
smelg <guivalenator@gmail.com>
2021-05-29 07:11:10 -0500

    reto2

---
+++ reto2.py
@@ -0,0 +1,17 @@
+#import os
+#RF01 Menu de Opciones
+opciones=["1. Cambiar contraseña", "2. Ingresar coordenadas actuales", "3. Ubica zona wifi más cercana","4. Guardar archivo con ubicación cercana","5. Actualizar registros de zonas wifi","6. Elegir opión de menú favorita", "7. Cerrar sesión"]
+
+contactor=True
+print(len(opciones)
+
+#def menu():
+#while True:
+#for i in range(len(opciones)):
+#    print(opciones[i])
+#break
+#   return"""
+
+#menu()
+#input("Elija una opción: ")
+#os.system("cls")
