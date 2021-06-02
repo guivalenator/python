b0bed82f3a111d76b431c38d22566e3e873c483a
SMELG <guivalenator@gmail.com>
2021-06-02 16:00:25 -0500

    *.py

--- reto2.1.py
+++ reto2.1.py
@@ -4,15 +4,14 @@ opciones=["Cambiar contraseña", "Ingresar coordenadas actuales", "Ubica zona wi
 d=0
 i=0
 j=7
-k=None
-l=None
-m=0
+#k=None
+#l=None
+#m=0
 c=0
 
 def inicio(j,d):
     global c
     while c<3:
-        #print(i)
         for i in range(j):
             print(i+1,".",opciones[i])
         print("")
@@ -24,43 +23,43 @@ def inicio(j,d):
         break
 
 def favoritos(c):
-    #k=0
-    #l=0
-    #m=0
     try:
         #while c < 3 :
+            #print(c)
             inicio(5,0)
             k=int(input("Seleccione opción favorita"))
             if k>0 and k<6 and c<4:
-                l=1
+                c=0
+                l=0
                 l=int(input("Para confirmar por favor responda:Si me giras pierdo tres unidades por eso debes colocarme siempre de pie, la respuesta es: "))
                 if l==9:
-                    m=1
+                    m=0
                     m=int(input("Para confirmar por favor responda:Soy un numero impar y despues del 5 me encontraras...la respuesta es: "))
                     if m==7:
-                        m=1
                         opciones.insert(0,opciones[k-1])
                         opciones.pop(k)
                         os.system("cls")
+                        c=0
                         inicio(7,1)
-                        c=4
                     else:
-                        c=4
-                        inicio(7)
+                        c=0
+                        inicio(7,1)
                 else:
-                    c=4
-                    inicio(7)
+                    os.system("cls")
+                    c=0
+                    inicio(7,1)
             else:
                 c=4
                 os.system("cls")
                 print("Error")
             #break
     except:
+        c=0
         os.system("cls")
         if l==1:
-            inicio(7,0)
+           inicio(7,1)
         elif m==1:
-            inicio(7,0)
+           inicio(7,1)
         else:
             os.system("cls")
             print("Error")
@@ -75,12 +74,15 @@ def menu():
         else:
             #os.system("cls")
             if op==6:
+                global c
+                c=0
                 favoritos(0)
             else:
                 os.system("cls")
                 if op ==7:
                     print("Hasta pronto")
                 else:
+                    input("...0")
                     print("Error44")
                     inicio(7,1)
     except:
