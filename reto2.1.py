3f856cfd382e7e60dfc885442dd72ad4e599dc6a
smelg <guivalenator@gmail.com>
2021-06-01 22:42:49 -0500

    *.py

--- reto2.1.py
+++ reto2.1.py
@@ -1,47 +1,48 @@
 import os
 #RF01 Menu de Opciones
 opciones=["Cambiar contraseña", "Ingresar coordenadas actuales", "Ubica zona wifi más cercana","Guardar archivo con ubicación cercana","Actualizar registros de zonas wifi","Elegir opión de menú favorita", "Cerrar sesión"]
-c=0
 d=0
 i=0
 j=7
 k=None
 l=None
 m=0
+c=0
 
 def inicio(j,d):
-    #os.system("cls")
-    while True:
+    global c
+    while c<3:
         #print(i)
         for i in range(j):
             print(i+1,".",opciones[i])
         print("")
+        if d==1:
+            c+=1
+            menu()
+        else:
+            h=0
         break
-    if d==1:
-        menu()
-    else:
-        c=0
-
 
 def favoritos(c):
-    k=0
-    l=0
-    m=0
+    #k=0
+    #l=0
+    #m=0
     try:
-        while c < 3 :
+        #while c < 3 :
             inicio(5,0)
             k=int(input("Seleccione opción favorita"))
             if k>0 and k<6 and c<4:
+                l=1
                 l=int(input("Para confirmar por favor responda:Si me giras pierdo tres unidades por eso debes colocarme siempre de pie, la respuesta es: "))
                 if l==9:
-                    l=1
+                    m=1
                     m=int(input("Para confirmar por favor responda:Soy un numero impar y despues del 5 me encontraras...la respuesta es: "))
                     if m==7:
                         m=1
                         opciones.insert(0,opciones[k-1])
                         opciones.pop(k)
                         os.system("cls")
-                        inicio(7,0)
+                        inicio(7,1)
                         c=4
                     else:
                         c=4
@@ -52,15 +53,17 @@ def favoritos(c):
             else:
                 c=4
                 os.system("cls")
-                print("Errorote")
+                print("Error")
+            #break
     except:
+        os.system("cls")
         if l==1:
-            inicio(7,1)
+            inicio(7,0)
         elif m==1:
-            inicio(7,1)
-        else: 
-            input("Error")
-            #inicio(7,1)
+            inicio(7,0)
+        else:
+            os.system("cls")
+            print("Error")
 
 def menu():
     try:
@@ -78,10 +81,13 @@ def menu():
                 if op ==7:
                     print("Hasta pronto")
                 else:
-                    print("Error numero invalido")         
+                    print("Error44")
+                    inicio(7,1)
     except:
+        os.system("cls")
         print("Error")
+        inicio(7,1)
 
-#os.system("cls")
+
+os.system("cls")
 inicio(7,1)
-
