765ec77473cd318a70f34765e2d1918a382e69c5
SMELG <guivalenator@gmail.com>
2021-06-09 15:54:44 -0500

    *.py

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+# Ingreso y conversion de numeros
+a=int(input("Ingrese el primer numero: "))
+b=int(input("Ingrese el segundo numero: "))
+
+#Calculo y proceso de salida del calculo de los datos ingresados
+print("La suma de", a ,"+", b, "es: ",a+b)
+
+print("La resta de", a ,"-", b, "es: ",a-b)
+
+print("La multiplicacion de", a ,"x", b, "es: ",a*b)
+
+print("La division de", a ,"entre", b, "es: ",a/b)
+
+print("La potencia de", a ,"a la", b, "es: ",a**b)
---
+++ promedio.py
@@ -0,0 +1,8 @@
+# Ingreso de Numerns
+a=int(input("Ingrese el primer numero: "))
+b=int(input(" INgrese el segundo numero: "))
+c=int(input(" INgrese el tercer numero: "))
+d=int(input(" INgrese el cuarto numero: "))
+e=int(input(" INgrese el quinto numerob: "))
+
+print(" EL promedio es", (a+b+d+d+e)/5)
