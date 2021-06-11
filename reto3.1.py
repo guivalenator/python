ecbf370364c4d499743d882e70fe19f84f148597
guivalenator <guivalenator@gmail.com>
2021-06-10 22:11:34 -0500

    *.py

--- reto3.1.py
+++ reto3.1.py
@@ -6,6 +6,7 @@ d=0
 j=7
 c=0
 s=0
+#password=None
 
 def login():
     os.system("cls")
@@ -21,6 +22,7 @@ def login():
 
     #RF02 Nombre de usuario y contraseña
     usuario="51675"
+    global passwd
     passwd="57615"
 
     user_ing=input("Ingrese su usuario: ")
@@ -39,6 +41,7 @@ def login():
                 # RF04 Confirmacion de ingreso al sistema
                 os.system ("cls") 
                 print("Sesión iniciada")
+                inicio(7,1)
             else:
                 os.system ("cls") 
                 print("Error")
@@ -49,17 +52,132 @@ def login():
         os.system ("cls") 
         print("Error")
 
+def inicio(j,d):
+                    global c
+                    while c<4:
+                        for i in range(j):
+                            print(i+1,".",opciones[i])
+                        print("")
+                        if d==1:
+                            c+=1
+                            menu()
+                        else:
+                            h=0
+                        break
+
+def menu():
+                    try:
+                        global s
+                        global c
+                        s=1
+                        op=int(input("Elija una opción"))
+                        print("")
+                        if op>0 and op<6:
+                            c=0
+                            os.system("cls")
+                            print("Usted ha elegido la opción",op)
+                            #pass_change()
+                            coordenadas()
+                        else:
+                            #os.system("cls")
+                            if op==6:
+                                c=0
+                                s=0
+                                os.system("cls")
+                                favoritos(0)
+                                os.system("exit")
+                            else:
+                                #RF05: El programa permite al usuario salir del menú
+                                os.system("cls")
+                                if op ==7:
+                                    print("Hasta pronto")
+                                else:
+                                    print("Error")
+                                    inicio(7,1)
+                    except:
+                            if s==0:
+                                print("Error")
+                                exit()
+                            else:
+                                os.system("cls")
+                                print("Error")
+                                inicio(7,1)
+
+def favoritos(c):
+                    try:
+                        inicio(5,0)
+                        #RF04 Opciones menu Favoritos
+                        k=int(input("Seleccione opción favorita"))
+                        if k>0 and k<6 and c<4:
+                            c=0
+                            l=1
+                            l=int(input("Para confirmar por favor responda:Soy un numero impar y despues del 5 me encontraras...la respuesta es: "))
+                            if l==7:
+                                m=1
+                                m=int(input("Para confirmar por favor responda:Soy un numero impar y antes del 7 me encontraras...la respuesta es: "))
+                                if m==5:
+                                    opciones.insert(0,opciones[k-1])
+                                    opciones.pop(k)
+                                    os.system("cls")
+                                    c=0
+                                    inicio(7,1)
+                                else:
+                                    c=0
+                                    os.system("cls")
+                                    print("Error")  
+                                    inicio(7,1)
+                            else:
+                                os.system("cls")
+                                print("Error")    
+                                c=0
+                                inicio(7,1)
+                        else:
+                            c=0
+                            os.system("cls")
+                            print("Error")
+                            inicio(7,1)
+                    except:
+                        c=0
+                        os.system("cls")
+                        if l==1:
+                            print("Error")
+                            inicio(7,1)
+                        elif m==1:
+                            print("Error")
+                            inicio(7,1)
+                        else:
+                            os.system("cls")
+                            print("Error")
+
+def coordenadas():
+    filas=3
+    columnas=2
+    coord=[[None]* columnas for i in range(filas)]
+    in_lat=input("Ingrese Latitud: ")
+    if in_lat.isnumeric==True:
+        round(float(in_lat),3)
+        lat=float(in_lat)
+        print(lat)
+    else:
+        print("Formato errado")
+
+    lon=input("Ingrese Longitud")
+    print(coord)
+
 def pass_change():
     global passwd
+    print(passwd)
     actual_pass=input("Ingrese la contraseña actual: ")
     if passwd==actual_pass:
         new_pass=input("Ingrese la nueva contraseña: ")
         if new_pass == passwd:
             exit()
         else:
+            passwd
             passwd=new_pass
             inicio(7,1)
     else:
         exit()
 
-login()
+#login()
+coordenadas()
